1f2f272af938a7d0cda9a807e7c5c551c1f04310
Tony <yuan210401@gmail.com>
2022-11-04 11:08:35 +0800

    TEST1104

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# project2
-project2
+#專案2
+##RBGLED燈光控制
+
+![總路圖](.0_1Button_RGBLED.png)
+
+
+###說明
+使用視窗程式控制RGB LED顯示的顏色
---
+++ main2.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+class Window(tk.Tk):
+    def __init__(self):
+        super().__init__()    
+        red = tk.Canvas(self,width=70,height=70,relief='ridge')
+        red.create_rectangle(10,10,60,60,fill="red")
+        red.bind('<ButtonRelease-1>',self.mouse_click)
+        red.grid(row=0, column=0)
+
+        green = tk.Canvas(self,width=70,height=70)
+        green.create_rectangle(10,10,60,60,fill="green")
+        green.grid(row=0, column=1)
+
+        blue = tk.Canvas(self,width=70,height=70)
+        blue.create_rectangle(10,10,60,60,fill="blue")
+        blue.grid(row=0, column=2)
+
+    def mouse_click(self,event):
+        print(event.__dict__)
+        event.widget.delete()
+        event.widget.create_rectangle(10,10,60,60,fill='white')
+        event.widget.create_rectangle(20,20,50,50,fill='red')
+               
+        event.widget.update()
+
+def main():
+    window = Window()
+    window.title("RGBLED 顏色控制")
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
